63fce1fc9531816a889b13e96198f2faa1bb0c91
vasuparsaniya <vasuparsaniya21@gmail.com>
2024-08-18 17:33:36 +0530

    project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { tsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  { rules: {} },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
+
