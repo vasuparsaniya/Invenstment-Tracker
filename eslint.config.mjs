c8a54f9dad1e3f90279972e6891fb8914c3fdc6a
vasuparsaniya <vasuparsaniya21@gmail.com>
2024-08-18 21:59:50 +0530

    husky setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tseslintPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
 
+const temp = {
+}
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { tsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
-  { rules: {} },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
-];
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+          tsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslintPlugin,
+      react: pluginReact,
+    },
+    rules: {
+      // JavaScript rules
+      "eqeqeq": "off",
+      "no-unused-vars": "error",
+      "max-len": ["warn", { code: 200 }],
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-use-before-define": "off",
 
+      // TypeScript rules
+      "@typescript-eslint/no-use-before-define": ["error"],
+
+      // React-specific rules
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
